bdeb7cd528d87dcb925576884f7fa57faa38fd93
Zhiyi Huang <hzy@cs.otago.ac.nz>
2014-06-18 16:42:55 +1200

    make a standard mailbox interface functions

--- source/console.c
+++ source/console.c
@@ -45,8 +45,6 @@ void setgpucolour(u16 c)
 
 uint initframebuf(uint width, uint height, uint depth)
 {
-    u8 *addr;
-
     fbinfo.width = width;
     fbinfo.height = height;
     fbinfo.v_width = width;
@@ -57,9 +55,7 @@ uint initframebuf(uint width, uint height, uint depth)
     fbinfo.y = 0;
     fbinfo.fbp = 0;
     fbinfo.fbs = 0;
-    addr = (u8 *)&fbinfo - KERNBASE;
-    addr += 0x40000000;
-    writemailbox((uint)addr, 1);
+    writemailbox((uint)&fbinfo, 1);
     return readmailbox(1);
 }
 
--- source/mailbox.c
+++ source/mailbox.c
@@ -22,26 +22,28 @@
 #define MAILBOX_BASE	0xFE00B880
 
 uint
-readmailbox(u8 c)
+readmailbox(u8 channel)
 {
 	uint x, y, z;
 
 again:
 	while ((inw(MAILBOX_BASE+24) & 0x40000000) != 0);
 	x = inw(MAILBOX_BASE);
-	z = x & 0xf; y = (uint)(c & 0xf);
+	z = x & 0xf; y = (uint)(channel & 0xf);
 	if((z ^ y) != 0) goto again;
 
 	return x&0xfffffff0;
 }
 
 void
-writemailbox(uint v, u8 c)
+writemailbox(uint addr, u8 channel)
 {
 	uint x, y;
 
-	x = v & 0xfffffff0;
-	y = x | (uint)(c & 0xf);
+	addr -= KERNBASE;   /* convert to ARM physical address */
+	addr += 0x40000000; /* convert to VC address space */
+	x = addr & 0xfffffff0;
+	y = x | (uint)(channel & 0xf);
 
 	while ((inw(MAILBOX_BASE+24) & 0x80000000) != 0);
 	outw(MAILBOX_BASE+32, y);
